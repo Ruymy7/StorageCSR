4c9b13d62db5d1960d1fd1643601cf80675d2a3d
Ruymán <ruyman.padronc@gmail.com>
2019-02-26 20:31:32 +0100

    [ADD] Router for api

--- routes/api.js
+++ routes/api.js
@@ -3,12 +3,12 @@ var router = express.Router();
 const fs = require('fs');
 
 /* GET users listing. */
-router.get('/video', function(req, res, next) {
-
+router.get('/videos', function(req, res, next) {
   res.send('respond with a resource');
 });
 
 router.post('/videoList', function(req, res, next){
+  const list = req.body.list;
   fs.linkSync();
 });
 
