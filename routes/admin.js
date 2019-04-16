b02e6513eb16eecec5823409b186acfc66ed6b6f
Ruymán <ruyman.padronc@gmail.com>
2019-04-16 18:56:29 +0100

    [ADD] CheckAdminToken

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,20 @@
-const express = require('express');
-const router = express.Router();
-const { exec } = require('child_process');
+const express       = require('express');
+const router        = express.Router();
+const nconf         = require('nconf');
+const { exec }      = require('child_process');
 
-router.post('/pull', function (req, res, next) {
+function checkAdminToken (req, res, next) {
+    const apiToken = nconf.get('adminToken');
+    const token = req.get('token');
+    if(!token || token !== apiToken){
+        return res.status(400).json({ error: "Wrong or missing token"});
+    }
+    next();
+}
+
+// ---------------- POSTS ------------------
+
+router.post('/pull', checkAdminToken, function (req, res, next) {
     exec('git pull', (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
